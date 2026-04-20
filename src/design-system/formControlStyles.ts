411ebf2fbfa3b4dc3c77b8a654438b080970d30c
mk <mk.eng1119@gmail.com>
2026-04-20 23:51:26 +0900

    Fix form control focus ring style

--- src/design-system/formControlStyles.ts
+++ src/design-system/formControlStyles.ts
@@ -1,5 +1,5 @@
 export const formControlFocusRingClass =
-  'focus:outline-2 focus:outline-offset-2 focus:outline-ring focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-ring';
+  'focus:outline-solid focus:outline-2 focus:outline-offset-2 focus:outline-ring focus-within:outline-solid focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-ring';
 
 export const formControlInvalidFocusRingClass =
   'focus:outline-destructive focus-within:outline-destructive';
